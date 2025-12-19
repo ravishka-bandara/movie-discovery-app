819b979305cad43275febc7fd1e2d4d95ebd2b0b
ravishka-bandara <ravishkabandara01@gmail.com>
2025-12-19 22:41:08 +0530

    feat: add dom elements

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const API_KEY = '';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
+// dom elements
+
+const moviesGrid = document.getElementById('moviesGrid');
+const searchInput = document.getElementById('searchInput');
+const searchBtn = document.getElementById('searchBtn');
+const sectionTitle = document.getElementById('sectionTitle');
+const movieModal = document.getElementById('movieModal');
+const modalBody = document.getElementById('modalBody');
+const closeBtn = document.getElementById('.close');
+
